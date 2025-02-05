19a10b7bca65c0769dd09776df4b4b8ada39728e
VasyS <rdy4future@gmail.com>
2025-02-06 01:15:18 +0300

    added simple http server for backend; updated tailwind in frontend to v4; added dockerfile and docker-compose files to run containers

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,5 +1,30 @@
-package server
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
+	r := http.NewServeMux()
 
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"status\": \"ok\"}"))
+	})
+
+	srv := &http.Server{
+		Addr:         "0.0.0.0:8082",
+		Handler:      r,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+		IdleTimeout:  time.Second * 10,
+	}
+
+	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("failed to start http server: %v", err)
+	}
 }
