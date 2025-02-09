8958d74735a7d3600c88b00a56067cd6dffff19e
VasyS <rdy4future@gmail.com>
2025-02-09 05:15:46 +0300

    optimize dockerfiles

--- backend/migrations/00001_create_container_status.sql
+++ backend/migrations/00001_create_container_status.sql
@@ -2,7 +2,6 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS container_status (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    container_id TEXT NOT NULL,
     ip TEXT NOT NULL,
     status TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL
