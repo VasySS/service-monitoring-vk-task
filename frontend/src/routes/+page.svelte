e9331259583d05d70ed32d85577d245432f1926c
VasyS <rdy4future@gmail.com>
2025-02-09 07:08:50 +0300

    updated frontend's visuals; updated readme

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -18,7 +18,7 @@
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center p-8">
-	<h1 class="pb-10">Container pinger</h1>
+	<h1 class="pb-10 text-2xl">Container pinger</h1>
 
 	{#if error}
 		<div class="mb-4 text-red-500">{error}</div>
@@ -29,7 +29,7 @@
 			<table class="min-w-full border-collapse bg-white">
 				<thead class="bg-gray-800 text-white">
 					<tr>
-						<th>IP</th>
+						<th class="p-1.5">IP</th>
 						<th>Status</th>
 						<th>Created at</th>
 						<th>Last success</th>
@@ -38,8 +38,8 @@
 
 				<tbody class="divide-y divide-gray-400">
 					{#each containerData as container}
-						<tr class="transition-colors hover:bg-gray-500">
-							<td>{container.ip}</td>
+						<tr class="text-center transition-colors hover:bg-gray-500">
+							<td class="p-1.5">{container.ip}</td>
 							<td>{container.status}</td>
 							<td>{new Date(container.createdAt).toLocaleString()}</td>
 							<td>{new Date(container.lastSuccess).toLocaleString()}</td>
